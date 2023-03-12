2b18a7f6a2c9be0719d7fa54a766b87f9d6e134b
Christian Widlund <christian.widlund@gmail.com>
2023-03-12 20:46:36 +0100

    base

--- api/index.php
+++ api/index.php
@@ -1,13 +1,39 @@
 <?php
-
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
 $app = AppFactory::create();
 
+/**
+ * The routing middleware should be added earlier than the ErrorMiddleware
+ * Otherwise exceptions thrown from it will not be handled by the middleware
+ */
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Middleware
+ *
+ * @param bool                  $displayErrorDetails -> Should be set to false in production
+ * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool                  $logErrorDetails -> Display error details in error log
+ * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
+ *
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+// Define app routes
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
@@ -15,13 +41,14 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 });
 
 $app->get('/hello/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Hello World!");
+    $response->getBody()->write('Hello World!');
     return $response;
 });
 
 $app->get('/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Index!");
+    $response->getBody()->write('Index!');
     return $response;
 });
 
+// Run app
 $app->run();
